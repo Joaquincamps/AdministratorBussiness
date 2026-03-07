c02813432fa0e30aede96d34aa714e6410344f1d
Joaquincamps <joaquin.campsmartinez@educa.madrid.org>
2026-03-07 13:04:06 +0100

    Commit para realizar la subida de los directorios,subdirectorios y ficheros del proyecto

---
+++ src/main/java/com/example/AdministratorBussiness/AdministratorBussinessApplication.java
@@ -0,0 +1,13 @@
+package com.example.AdministratorBussiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdministratorBussinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdministratorBussinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AdministratorBussiness/AdministratorBussinessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AdministratorBussiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdministratorBussinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
